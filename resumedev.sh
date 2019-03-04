410ed8857fb8b24a237bc815efff7f76da2ef4a7
Ushiro Katsuhiro <ushiro.katsuhiro@gmail.com>
2019-03-03 21:17:54 -0500

    Change.log
    - Changed radiorun_lwa.sh to radiotrans_run.sh

--- resumedev.sh
+++ resumedev.sh
@@ -6,5 +6,5 @@
 # PURPOSE: Short script to allow me to quickly reopen files in vim and resume any work that I was doing
 #           previously.
 
-vim -p change.log resumedev.sh radioreduce.sh radiofilter.sh radiotransfer.sh radiorun_lwa.sh \
+vim -p change.log resumedev.sh radioreduce.sh radiofilter.sh radiotransfer.sh radiotrans_run.sh \
          radiosearch.sh rfibandpass.py dv.py apputils.py
